0a4cd0052bd7b1e5c820077b39fbeb94da658c9c
DrAymeinstein <152915916+DrAymeinstein@users.noreply.github.com>
2023-12-18 21:42:29 +0100

    Create MergedSortedArray.cpp

---
+++ MergedSortedArray.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
+#include <vector>
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int> nums2, int n) {
+        int i = m - 1;       // starts at the last valid element in nums1
+        int j = n - 1;       // starts at the last element in nums2.
+        int k = m + n - 1;   // starts at the last position of the merged array.
+
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else                     nums1[k--] = nums2[j--];
+        }
+
+        while (j >= 0) nums1[k--] = nums2[j--];
+    }
+};
