185bb6561b2f5f18e0f03fd32b63cb38902ea300
DrAymeinstein <152915916+DrAymeinstein@users.noreply.github.com>
2023-12-19 19:29:13 +0100

    Update ChristmasChallenge(GFSS P1).cpp

--- ChristmasChallenge(GFSS P1).cpp
+++ ChristmasChallenge(GFSS P1).cpp
@@ -1,2 +1,16 @@
 // https://dmoj.ca/problem/gfsscc21p1
 // GFSS Christmas Challenge P1 - Coal Counting
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+
+	return 0;
+}
